ee7be2ecdacee095de49cd6c88030c704a56c294
kris-J <fangjie-0811@163.com>
2018-02-28 16:18:50 +0800

    session favicon https

--- src/main/java/com/fangjie/girl/GirlApplication.java
+++ src/main/java/com/fangjie/girl/GirlApplication.java
@@ -11,4 +11,34 @@ public class GirlApplication {
     }
 
 
+    //配置http自动转向https
+//    @Bean
+//    public EmbeddedServletContainerFactory servletContainer(){
+//        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory(){
+//            protected void postProcessContext(Context context){
+//                SecurityConstraint securityConstraint = new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//
+//        tomcat.addAdditionalTomcatConnectors(httpConnector());
+//        return tomcat;
+//    }
+//
+//    @Bean
+//    public Connector httpConnector(){
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        //Connector监听的http的端口号
+//        connector.setPort(8090);
+//        connector.setSecure(false);
+//        //监听到http的端口号后转向到的https的端口号
+//        connector.setRedirectPort(8443);
+//        return connector;
+//    }
+
 }
